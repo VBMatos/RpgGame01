b485c128943e3cdbad4a990fae4abd1a286af6db
Vitor Batista Matos <vitorbatistamatos@gmail.com>
2023-07-09 13:41:31 -0300

    Atualiza base do game para Kotlin

---
+++ lwjgl3/src/main/kotlin/com/vbm/game/lwjgl3/Lwjgl3Launcher.kt
@@ -0,0 +1,19 @@
+@file:JvmName("Lwjgl3Launcher")
+
+package com.vbm.game.lwjgl3
+
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
+import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
+import com.vbm.game.MyGame
+
+/** Launches the desktop (LWJGL3) application. */
+fun main() {
+    // This handles macOS support and helps on Windows.
+    if (StartupHelper.startNewJvmIfRequired())
+      return
+    Lwjgl3Application(MyGame(), Lwjgl3ApplicationConfiguration().apply {
+        setTitle("RpgGame01")
+        setWindowedMode(640, 480)
+        setWindowIcon(*(arrayOf(128, 64, 32, 16).map { "libgdx$it.png" }.toTypedArray()))
+    })
+}
